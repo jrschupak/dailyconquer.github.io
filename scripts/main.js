ef4a02ea2483b974b5946818291693cacb490b47
Jonathan Schupak <jrschupak@gmail.com>
2019-08-04 13:31:25 -0400

    Enter/Yes btn and lifegoals page

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,15 @@
 window.addEventListener('load', (event) => {
 	console.log('main.js is loaded and connected')
 })
+
+
+var enterBtn = document.getElementsByClassName("enter"); // Find the paragraph element in the page
+  
+  enterBtn.onclick = function() {
+  	console.log('enter button on click works')
+  }; // Add onclick function to element
+    
+  
+window.onlaod() {
+	console.log('window onload console.log works')
+}
